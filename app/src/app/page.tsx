0d100f7fc9720352a3e74641a413c91b18b2a5a3
fenaum <hnguyen4497@outlook.com>
2024-01-18 15:03:41 -0800

    added shadcn

---
+++ app/src/app/page.tsx
@@ -0,0 +1,57 @@
+import { Card, CardHeader, CardTitle, CardDescription, }
+from "../components/ui/card"
+
+interface VaccinationStatus {
+  rabies: boolean;
+  distemper: boolean;
+  felineLeukemia: boolean;
+}
+
+interface CatOwner {
+  name: string;
+  contact: string;
+  address: string;
+}
+
+interface CatProfile {
+  catProfile: {
+    name: string;
+    id: number;
+    age: number;
+    breed: string;
+    color: string;
+    weight: number;
+    gender: string;
+    personality: string[];
+    vaccinations: VaccinationStatus;
+    owner: CatOwner;
+    images: string[];
+  };
+}
+
+async function getCatProfile(): Promise<CatProfile[]> {
+  const result = await fetch('http://localhost:4000/cats')
+  return result.json();
+}
+
+export default async function Home() {
+  const cats = await getCatProfile();
+
+ return (
+   <main>
+     {cats.map((cat) => (
+       <Card key={cat.catProfile.id}>
+         <CardHeader>
+           <div>
+             <CardTitle>{cat.catProfile.name}</CardTitle>
+             <CardDescription>
+               {cat.catProfile.color}{' '}{cat.catProfile.gender}{" "}
+               {cat.catProfile.breed}
+             </CardDescription>
+           </div>
+         </CardHeader>
+       </Card>
+     ))}
+   </main>
+ );
+}
