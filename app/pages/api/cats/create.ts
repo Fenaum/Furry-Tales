a3aea75918280185fb7636a068bd1eab228870de
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2024-02-13 05:35:30 -0800

    added get all route

--- app/pages/api/cats/create.ts
+++ app/pages/api/cats/create.ts
@@ -1,19 +1,10 @@
-// pages/api/cats/create.ts
-
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Cat } from "../../../src/models/Cat"; // Adjust the import path according to your project structure
+import { Cat } from "../../../src/models/Cat";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    // Cast the request body to the Cat type
-    const newCat: Cat = req.body as Cat;
-
-    // Perform validation checks on newCat if necessary
-    // ...
-
-    // Process the newCat object (e.g., save to database)
-    // ...
-
+    const newCat: Cat = req.body; // Assuming the request body is properly typed
+    // Validate and process the newCat object...
     res.status(200).json({ message: "Cat created successfully." });
   } else {
     res.setHeader("Allow", ["POST"]);
