8ded4b2de31ec84d2b19e1e9f7b0fdb8cb8a6560
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2024-03-20 14:12:48 -0700

    adding uploading functions

---
+++ app/src/lib/storageService.ts
@@ -0,0 +1,25 @@
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
+
+const storage = getStorage();
+
+async function uploadCatPhoto(catId: string, photoFile: File) {
+  // Create a storage reference
+  const storageRef = ref(storage, `cats/${catId}/${photoFile.name}`);
+
+  // Upload the file
+  await uploadBytes(storageRef, photoFile);
+
+  // Get the download URL
+  const photoURL = await getDownloadURL(storageRef);
+
+  return photoURL;
+}
+
+async function saveCatPhotoURL(catId: string, photoURL: string) {
+  const catRef = doc(collection(db, "cats"), catId);
+  await setDoc(catRef, { photoURL }, { merge: true });
+}
+
+export { uploadCatPhoto, saveCatPhotoURL } ;
